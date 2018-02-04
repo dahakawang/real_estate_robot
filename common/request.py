6e1c10ea635cf39a3134926830e8d03505d938bc
David <david.kaiqiang.wang@gmail.com>
2018-02-04 19:33:54 +0800

    differentiate http return codes

--- common/request.py
+++ common/request.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from common.errors import HttpFetchError
+from common.errors import *
 
 
 #TODO support proxy
@@ -14,6 +14,16 @@ def get_soup(url):
         # print("requesting {}".format(url))
         request = Request()
         resp = request.get(url)
+
+        if resp.status_code == 404:
+            raise KnownError("not found error (404)".format(url))
+        if resp.status_code != 200:
+            raise HttpFetchError("remote server returned {}".format(resp.status_code))
+
         return BeautifulSoup(resp.text, "lxml")
     except Exception as e:
         raise HttpFetchError() from e
+
+
+if __name__ == "__main__":
+    get_soup("https://sh.lianjia.com/ershoufang/107002002775.html")
